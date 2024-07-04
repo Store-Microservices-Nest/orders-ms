5b7d8ecf42fb977e0b598625f1fd615a0211e070
Yalid Anthony Guevara Castro <yalidanthony@gmail.com>
2024-07-03 21:04:20 -0600

    Error and types  handling

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
@@ -17,8 +17,15 @@ async function bootstrap() {
 
   const logger = new Logger('Orders-Main');
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   app.listen();
-  
+
   logger.log(`Orders-Main running on port ${envs.port}`);
 }
 bootstrap();
--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,8 +1,14 @@
-import { Controller, NotImplementedException } from '@nestjs/common';
+import {
+  Controller,
+  NotImplementedException,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { OrderPaginationDto } from './dto/order-pagination.dto';
+import { ChangeOrderStatusDto } from './dto';
 
 @Controller()
 export class OrdersController {
@@ -14,18 +20,21 @@ export class OrdersController {
   }
 
   @MessagePattern('findAllOrders')
-  findAll() {
-    return this.ordersService.findAll();
+  findAll(@Payload() orderPaginationDto: OrderPaginationDto) {
+    return this.ordersService.findAll(orderPaginationDto);
   }
 
   @MessagePattern('findOneOrder')
-  findOne(@Payload('id') id: number) {
+  findOne(@Payload('id', ParseUUIDPipe) id: string) {
     return this.ordersService.findOne(id);
   }
 
-  // @MessagePattern('changeOrderStatus')
-  // changeOrderStatus() {
-  //   // return this,this.ordersService.updateOrder()
-  //   throw new NotImplementedException();
-  // }
+  @MessagePattern('changeOrderStatus')
+  changeOrderStatus(
+    @Payload()
+    changeOrderStatusDto: ChangeOrderStatusDto,
+  ) {
+    // return this,this.ordersService.updateOrder()
+    return this.ordersService.changeStatus(changeOrderStatusDto);
+  }
 }
