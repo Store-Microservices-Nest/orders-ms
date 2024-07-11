6753121c62333cb936c9869c8664262bd3db337b
Yalid Anthony Guevara Castro <yalidanthony@gmail.com>
2024-07-11 10:48:17 -0600

    Containerization

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,16 +3,16 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
-  PRODUCT_MICROSERVICE_HOST: string;
-  PRODUCT_MICROSERVICE_PORT: number;
+  // PRODUCT_MICROSERVICE_HOST: string;
+  // PRODUCT_MICROSERVICE_PORT: number;
   NATS_SERVERS: string[];
 }
 
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
-    PRODUCT_MICROSERVICE_HOST: joi.string().required(),
-    PRODUCT_MICROSERVICE_PORT: joi.number().required(),
+    // PRODUCT_MICROSERVICE_HOST: joi.string().required(),
+    // PRODUCT_MICROSERVICE_PORT: joi.number().required(),
     NATS_SERVERS: joi.array().items(joi.string().required()),
   })
   .unknown(true);
@@ -30,7 +30,7 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
-  productMicroserviceHost: envVars.PRODUCT_MICROSERVICE_HOST,
-  productMicroservicePort: envVars.PRODUCT_MICROSERVICE_PORT,
+  // productMicroserviceHost: envVars.PRODUCT_MICROSERVICE_HOST,
+  // productMicroservicePort: envVars.PRODUCT_MICROSERVICE_PORT,
   natsServers: envVars.NATS_SERVERS,
 };
