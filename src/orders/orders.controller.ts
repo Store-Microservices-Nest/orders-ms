03da2b8e2cfb1a79bd0c9b5c179fece5f1d0f790
Yalid Anthony Guevara Castro <yalidanthony@gmail.com>
2024-07-15 11:16:20 -0600

    Emit payment.succeeded event

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -3,7 +3,7 @@ import {
   NotImplementedException,
   ParseUUIDPipe,
 } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { MessagePattern, Payload, EventPattern } from '@nestjs/microservices';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
@@ -41,5 +41,13 @@ export class OrdersController {
   ) {
     // return this,this.ordersService.updateOrder()
     return this.ordersService.changeStatus(changeOrderStatusDto);
+  }
+
+  @EventPattern('payment.succeeded')
+  paidOrder(@Payload() paidOrderDto: any){
+    console.log('PaymentOrderDto', paidOrderDto)
+
+    return paidOrderDto;
+
   }
 }
